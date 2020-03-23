80b9d61d33a83f38e87e90e94195ee2c619fba40
Conterry <glebois05@gmail.com>
2020-03-23 20:11:41 +0200

    modifi

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[HideInInspector]
+	public int s;
+
+	[SerializeField] 
+	private int a;
+
+	internal int b;
+
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
